72b89469b7a633bbffaa5ca5b15408f65c46c3e5
jooedvardtanar <joo.edvard@tanar.szamalk-szalezi.hu>
2024-04-27 11:43:27 +0200

    feladat-1

---
+++ contants/api.js
@@ -0,0 +1,2 @@
+const pokemon_URL = 'https://pokeapi.co/api/v2/pokemon/{id or name}';
+export {pokemon_URL}
--- script.js
+++ script.js
@@ -1,13 +1,2 @@
-import { fetchDataFromURL, init, readInTranslation } from "./script/orai.js";
 
-const pokemon = 'https://pokeapi.co/api/v2/pokemon/';
-const IMINEK = 'https://pokeapi.co/api/v2/pokemon/ditto';
-
-init();
-readInTranslation();
-readInTranslation('en');
-const pokemonData = await fetchDataFromURL(pokemon);
-console.log(pokemonData)
-
-// Input + Gomb pl.: ditto
-// fénykép az oldalra
+// itt dolgozz
